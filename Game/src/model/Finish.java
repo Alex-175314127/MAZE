0cb0ea1a419359055bc512223d2989311508b2d1
Alexander Adam <Alexander Adam@192.168.1.1>
2018-12-17 19:37:20 +0700

    versi x8.14

--- Game/src/model/Finish.java
+++ Game/src/model/Finish.java
@@ -6,18 +6,20 @@ import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 
-public class Finish extends Sel{
+public class Finish extends Sel{//// kelas bawaan dari kelas Sel
     /**
-     * berfungsi menentukan finish dan membuat gambarnya berubah sesuai file
-     * ynag telah di simpan
+     * constructor Finish Pada saat objek Finish dibuat, kita memberikan 2 nilai untuk
+     * konstruktor yang nantinya akan digunakan untuk memberi nilai pada
+     * attribut x dan y di class. Kesimpulannya, pada saat objek Finish dibuat, objek
+     * tersebut sudah memiliki nilai x dan y.
      * @param x
      * @param y 
      */
     public Finish(int x, int y){
         super(x, y);
         
-        URL location = this.getClass().getResource("finish.png");
-        ImageIcon gambar = new ImageIcon(location);        
-        this.setImage(gambar.getImage());
+        URL location = this.getClass().getResource("finish.png");// memanggil gambar dengan nama file sesuai yang terdapat pada file
+        ImageIcon gambar = new ImageIcon(location);      // mendeklarasikan object baru  
+        this.setImage(gambar.getImage());//memanggil method getimage milik gambar melalui method setimage
     }
 }
