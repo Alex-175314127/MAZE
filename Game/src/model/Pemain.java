f56410ccad812b0d9f5e0730b81c12cdec5d4617
Alexander Adam <Alexander Adam@192.168.1.1>
2018-12-17 19:39:13 +0700

    versi x8.14

--- Game/src/model/Pemain.java
+++ Game/src/model/Pemain.java
@@ -4,13 +4,20 @@ import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 
-public class Pemain extends Sel {
+public class Pemain extends Sel {// kelas bawaan dari kelas Sel
     
      public Pemain(int x, int y) {
+         /**
+     * constructor Pemain Pada saat objek pemain dibuat, kita memberikan 2 nilai untuk
+     * konstruktor yang nantinya akan digunakan untuk memberi nilai pada
+     * attribut x dan y di class. Kesimpulannya, pada saat objek pemain dibuat, objek
+     * tersebut sudah memiliki nilai x dan y.
+     *
+     */
         super(x, y);
-        URL location = this.getClass().getResource("player.jpg");
-        ImageIcon gambar = new ImageIcon(location);        
-        this.setImage(gambar.getImage());
+        URL location = this.getClass().getResource("player.jpg");// memanggil gambar dengan nama file sesuai yang terdapat pada file
+        ImageIcon gambar = new ImageIcon(location);       // mendeklarasikan object baru     
+        this.setImage(gambar.getImage());//memanggil method getimage milik gambar melalui method setimage
     }
 
     public void Gerak(int x,int y) {        
@@ -20,6 +27,6 @@ public class Pemain extends Sel {
 }
 
     int size() {
-        throw new UnsupportedOperationException("Failed");
+        throw new UnsupportedOperationException("Failed");// menampilkan pesan Failed
     }
 }
